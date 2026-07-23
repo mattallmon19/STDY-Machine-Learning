bd12562b23ecfa2dd2ffa8cf9a624c80656c2d51
mattallmon19 <mattallmon19@gmail.com>
2026-07-23 15:32:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 Machine Learning')
+st.title('Machine Learning')
 
 st.write('This application is to demonstrate Machine Learning by predicting the future price of gold!')
