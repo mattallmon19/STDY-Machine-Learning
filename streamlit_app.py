14edd5efc4f0795814c13682bbd21a8566cd1ce8
mattallmon19 <mattallmon19@gmail.com>
2026-07-23 15:40:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import panda as pd
 
 st.title('Machine Learning')
 
 st.write('This application is to demonstrate Machine Learning by predicting the future price of gold!')
+
+df = pd.read_csv()
