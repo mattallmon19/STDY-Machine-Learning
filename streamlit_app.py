906a1d111c463ca391a8fb2e82af35a6ca8157e8
mattallmon19 <mattallmon19@gmail.com>
2026-07-23 18:15:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,51 @@
-import streamlit as st
-import panda as pd
+# Import Packages
+from datetime import datetime
+import http.client
+import mimetypes
+import json
+import csv
 
-st.title('Machine Learning')
+# Timestamp
+now = datetime.now()
+today = datetime.today()
+print("Date Success!")
 
-st.write('This application is to demonstrate Machine Learning by predicting the future price of gold!')
+# Credentials
+a = open('./cred.json')
+creds = json.load(a)
+token = creds['token']
+content = creds['type']
+url = creds['url']
+base = creds['base']
+symbol = creds['symbol']
+print("Credentials Success!")
 
-with st.expander('Data'):
-                 st.write('Raw Data')
-df = pd.read_csv()
-df
+# Get Data
+conn = http.client.HTTPSConnection(url)
+payload = ''
+headers = {
+    'x-access-token': f'{token}',
+    'Content-Type': f'{content}'
+}
+conn.request("GET", f"/api/{base}/{symbol}", payload, headers)
+res = conn.getresponse()
+print("GET Success!")
+
+# Status
+print(f"API Status: {res.status}")
+
+# Result from JSON
+data = json.loads(res.read())
+result = data['price']
+
+# Result
+print(result)
+
+# Result To CSV
+with open("../data/goldprices.csv", "a+", newline="") as f:
+    writer = csv.writer(f, delimiter=",")
+    writer.writerow([now, result])
+
+print("File Success!")
 
 
